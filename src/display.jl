9cacbb15071be8df4be9106ee5847611ce58c693
Jeffrey Lin <jeffrey@icurse.nl>
2018-03-18 02:16:24 -0400

    display.jl: add a function for plotting points

--- src/display.jl
+++ src/display.jl
@@ -3,11 +3,15 @@ module Display
 
 using Config
 
-export dump_ppm
+export dump_ppm, plot!
 
 function dump_ppm(data, file=STDOUT)
     write(file, "$MAGICNUMBER $(size(data, 4)) $(size(data, 3))  $MAXCOLOR\n")
     writedlm(file, data)
+end
+
+function plot!(data, point, color)
+    data[:, point[1], point[2], point[3]] = color
 end
 end
 
