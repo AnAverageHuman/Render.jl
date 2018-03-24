6a7a0cd8a07a6ff38b540068506fd2da7c53cee6
Jeffrey Lin <jeffrey@icurse.nl>
2018-03-24 00:39:17 -0400

    display.jl: check for bounds in plot!()
    Also move the origin to the bottom right.

--- src/display.jl
+++ src/display.jl
@@ -11,6 +11,11 @@ function dump_ppm(data, file=STDOUT)
 end
 
 function plot!(data, point, color)
+    # origin should be in lower left instead of upper left
+    point = point[1], point[3], size(data, 3) - point[2] - 1
+    1 < point[2] < size(data, 4) || return
+    1 < point[3] < size(data, 3) || return
+
     data[:, point[1], point[2], point[3]] = color
 end
 end
