3486398f6bec0c1788a39dd2e2c43a4ceb626267
Jeffrey Lin <jeffrey@icurse.nl>
2018-04-05 18:33:55 -0400

    main.jl: Remove redundant anon func in call to map

--- src/main.jl
+++ src/main.jl
@@ -21,21 +21,24 @@ function parsefile(f::IOStream)
     items = vec(permutedims(items, (2, 1)))
     filter!(x -> x != "", items)
 
+    tmp = zeros(10)
+    floats = fill(Float64, 10) # vector of 10 Float64s
+
     while ! isempty(items)
         command = shift!(items)
         if command == "line"
-            tmp = map(x -> parse(Float64, x), splice!(items, 1:6))
+            map!(parse, tmp, floats, splice!(items, 1:6))
             addedge!(edges, tmp[1:3], tmp[4:6])
         elseif command == "circle"
-            tmp = map(x -> parse(Float64, x), splice!(items, 1:4))
+            map!(parse, tmp, floats, splice!(items, 1:4))
             addcircle!(edges, tmp[1:3], tmp[4], CIRCSTEPS)
         elseif command == "ident"
             transmat = eye(4)
         elseif command == "scale"
-            tmp = map(x -> parse(Float64, x), splice!(items, 1:3))
+            map!(parse, tmp, floats, splice!(items, 1:3))
             transmat = mkscale(tmp[1:3]) * transmat
         elseif command == "move"
-            tmp = map(x -> parse(Float64, x), splice!(items, 1:3))
+            map!(parse, tmp, floats, splice!(items, 1:3))
             transmat = mktranslate(tmp[1:3]) * transmat
         elseif command == "rotate"
             dir = shift!(items)
