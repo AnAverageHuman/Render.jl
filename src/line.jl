2435abe405196713caa3d193baacc5a881566168
Jeffrey Lin <jeffrey@icurse.nl>
2018-04-29 16:06:08 -0400

    line.jl: calculate and plot z-values for points

--- src/line.jl
+++ src/line.jl
@@ -6,12 +6,15 @@ using Display: plot!
 export drawline!
 
 function drawline!(data, p1, p2, color)
-    global dx, dy, x, y
+    global dx, dy, dz, x, y, z
     all(isfinite.(p1)) && all(isfinite.(p2)) || return    # refuse to draw NaN and Inf
+
     x = p1[1]
     y = p1[2]
+    z = p1[3]
     dx = p2[1] - p1[1]
     dy = p2[2] - p1[2]
+    dz = p2[3] - p1[3]
 
     if abs(dy) < abs(dx)
         if p1[1] > p2[1]
@@ -29,7 +32,7 @@ function drawline!(data, p1, p2, color)
 end
 
 function drawlinelow!(data, mx, color)
-    global dx, dy, x, y
+    global dx, dy, dz, x, y, z
     yi = 1
     if dy < 0
         yi = -1
@@ -37,9 +40,12 @@ function drawlinelow!(data, mx, color)
     end
 
     d = 2dy - dx
+    dist = abs(mx - x)
+    dz = dist > 0 ? dz / dist : 0
     for i in x:mx
-        plot!(data, [1, y, i], color)
+        plot!(data, [z, y, i], color)
         d += 2dy
+        z += dz
 
         if d > 0
             y += yi
@@ -49,7 +55,7 @@ function drawlinelow!(data, mx, color)
 end
 
 function drawlinehigh!(data, my, color)
-    global dx, dy, x, y
+    global dx, dy, dz, x, y, z
     xi = 1
     if dx < 0
         xi = -1
@@ -57,9 +63,13 @@ function drawlinehigh!(data, my, color)
     end
 
     d = 2dx -dy
+    dist = abs(my - y)
+    dz = dist > 0 ? dz / dist : 0
+
     for i in y:my
-        plot!(data, [1, i, x], color)
+        plot!(data, [z, i, x], color)
         d += 2dx
+        z += dz
 
         if d > 0
             x += xi
