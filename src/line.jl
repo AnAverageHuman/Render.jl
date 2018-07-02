e26c934e65595a0c50b0133a6d93ed37dd8de6b7
Jeffrey Lin <jeffrey@icurse.nl>
2018-07-02 11:53:16 -0400

    line.jl: inline drawline(high/low); avoid globals
    Also fix a small type change issue by assigning zero(x) instead of 0.
    
    julia> judge(median(staging), median(master))
    BenchmarkTools.TrialJudgement:
      time:   -5.61% => improvement (5.00% tolerance)
      memory: -1.62% => improvement (1.00% tolerance)

--- src/line.jl
+++ src/line.jl
@@ -1,6 +1,5 @@
 function drawline!(data::IBuffer, p1::Vector{Float64}, p2::Vector{Float64},
                    color::Vector{Float64})
-    global dx, dy, dz, x, y, z
     all(isfinite.(p1)) && all(isfinite.(p2)) || return    # refuse to draw NaN and Inf
 
     x = p1[1]
@@ -13,61 +12,53 @@ function drawline!(data::IBuffer, p1::Vector{Float64}, p2::Vector{Float64},
     if abs(dy) < abs(dx)
         if p1[1] > p2[1]
             drawline!(data, p2, p1, color)
-        else
-            drawlinelow!(data, p2[1], color)
+        else    # drawline: low
+            mx = p2[1]
+            yi = 1
+            if dy < 0
+                yi = -1
+                dy = -dy
+            end
+
+            d = 2dy - dx
+            dist = abs(mx - x)
+            dz = dist > 0 ? dz / dist : 0
+            for i in x:mx
+                plot!(data, [z, y, i], color)
+                d += 2dy
+                z += dz
+
+                if d > 0
+                    y += yi
+                    d -= 2dx
+                end
+            end
         end
     else
         if p1[2] > p2[2]
             drawline!(data, p2, p1, color)
-        else
-            drawlinehigh!(data, p2[2], color)
-        end
-    end
-end
+        else    # drawline: high
+            my = p2[2]
+            xi = 1
+            if dx < 0
+                xi = -1
+                dx = -dx
+            end
 
-function drawlinelow!(data::IBuffer, mx::Float64, color::Vector{Float64})
-    global dx, dy, dz, x, y, z
-    yi = 1
-    if dy < 0
-        yi = -1
-        dy = -dy
-    end
+            d = 2dx -dy
+            dist = abs(my - y)
+            dz = dist > 0 ? dz / dist : zero(dz)
 
-    d = 2dy - dx
-    dist = abs(mx - x)
-    dz = dist > 0 ? dz / dist : 0
-    for i in x:mx
-        plot!(data, [z, y, i], color)
-        d += 2dy
-        z += dz
+            for i in y:my
+                plot!(data, [z, i, x], color)
+                d += 2dx
+                z += dz
 
-        if d > 0
-            y += yi
-            d -= 2dx
-        end
-    end
-end
-
-function drawlinehigh!(data::IBuffer, my::Float64, color::Vector{Float64})
-    global dx, dy, dz, x, y, z
-    xi = 1
-    if dx < 0
-        xi = -1
-        dx = -dx
-    end
-
-    d = 2dx -dy
-    dist = abs(my - y)
-    dz = dist > 0 ? dz / dist : 0
-
-    for i in y:my
-        plot!(data, [z, i, x], color)
-        d += 2dx
-        z += dz
-
-        if d > 0
-            x += xi
-            d -= 2dy
+                if d > 0
+                    x += xi
+                    d -= 2dy
+                end
+            end
         end
     end
 end
